a14b1c590801b36f3041c0028669af0c0697ecbb
d.zhukovskiy <d.zhukovskiy@tbank.ru>
2025-05-15 07:09:53 +0300

    Fixes

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,11 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { apiFetchPublic } from "../common/api";
-import { Post } from "../common/types";
+import { Domain, Post } from "../common/types";
+import { useAuth } from "./useAuth";
 
 export const usePosts = () => {
+  const { user } = useAuth();
+
   return useQuery<Post[]>({
-    queryKey: ["posts"],
-    queryFn: () => apiFetchPublic<Post[]>("/post"),
+    queryKey: ["posts", user?.domain],
+    queryFn: async () => {
+      let domains = user?.domain;
+      if (domains === undefined || domains.length == 0) {
+        domains = [Domain.MATHEMATICS, Domain.CS, Domain.DB]
+      }
+      return apiFetchPublic<Post[]>("/post/filter", {
+        method: "POST",
+        body: JSON.stringify({ domains }),
+      });
+    },
   });
 };
 
